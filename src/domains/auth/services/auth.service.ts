4f9b80360e9442354ba6f556bf5966f7624d20e3
crumafood-create <crumafood@gmail.com>
2026-05-15 01:00:55 -0600

    Create auth.service.ts

---
+++ src/domains/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import {
+  loginRepository,
+  logoutRepository
+}
+from '../repositories/auth.repository';
+
+export async function loginService(
+  email: string,
+  password: string
+) {
+
+  return loginRepository(
+    email,
+    password
+  );
+}
+
+export async function logoutService() {
+
+  return logoutRepository();
+}

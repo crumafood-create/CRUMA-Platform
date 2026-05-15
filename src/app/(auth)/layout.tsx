d69c363184d5f6a904ef0692396600a519e40338
crumafood-create <crumafood@gmail.com>
2026-05-14 23:55:15 -0600

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,1 +1,17 @@
+import type { ReactNode } from 'react';
 
+export default function AuthLayout({
+  children
+}: {
+  children: ReactNode;
+}) {
+
+  return (
+
+    <div>
+
+      {children}
+
+    </div>
+  );
+}

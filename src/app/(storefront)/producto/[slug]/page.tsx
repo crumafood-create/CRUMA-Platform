da26b01acdda41501e058dfa008870c4a0134755
crumafood-create <crumafood@gmail.com>
2026-05-14 23:52:11 -0600

    Create page.tsx

---
+++ src/app/(storefront)/producto/[slug]/page.tsx
@@ -0,0 +1,24 @@
+interface Props {
+
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function ProductPage({
+  params
+}: Props) {
+
+  const { slug } = await params;
+
+  return (
+
+    <main>
+
+      <h1>
+        {slug}
+      </h1>
+
+    </main>
+  );
+}

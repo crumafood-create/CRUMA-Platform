acafd567c6c4d0f03e3751346ce27a95bbd2bd6f
crumafood-create <crumafood@gmail.com>
2026-05-15 10:16:57 -0600

    Update and rename .tsx to inventory-table.tsx

---
+++ src/domains/inventory/components/inventory-table.tsx
@@ -0,0 +1,93 @@
+import type { InventoryLevel }
+from '../types/inventory-level.type';
+
+interface Props {
+
+  inventory: InventoryLevel[];
+}
+
+export function InventoryTable({
+  inventory
+}: Props) {
+
+  return (
+
+    <div className="overflow-hidden rounded-2xl border">
+
+      <table className="w-full">
+
+        <thead className="border-b bg-gray-50">
+
+          <tr>
+
+            <th className="p-4 text-left">
+
+              Product ID
+
+            </th>
+
+            <th className="p-4 text-left">
+
+              Warehouse
+
+            </th>
+
+            <th className="p-4 text-left">
+
+              Disponible
+
+            </th>
+
+            <th className="p-4 text-left">
+
+              Reservado
+
+            </th>
+
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          {inventory.map(item => (
+
+            <tr
+              key={item.id}
+              className="border-b"
+            >
+
+              <td className="p-4">
+
+                {item.product_id}
+
+              </td>
+
+              <td className="p-4">
+
+                {item.warehouse_id}
+
+              </td>
+
+              <td className="p-4">
+
+                {item.available_quantity}
+
+              </td>
+
+              <td className="p-4">
+
+                {item.reserved_quantity}
+
+              </td>
+
+            </tr>
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+  );
+}

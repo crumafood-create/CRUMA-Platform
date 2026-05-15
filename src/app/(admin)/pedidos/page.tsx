676273b25dbf63412503d3d18b1f5696e6588eef
crumafood-create <crumafood@gmail.com>
2026-05-15 09:56:42 -0600

    Update page.tsx

--- src/app/(admin)/pedidos/page.tsx
+++ src/app/(admin)/pedidos/page.tsx
@@ -1,12 +1,30 @@
-export default function OrdersPage() {
+import { fetchOrders }
+from '@/domains/orders/services/orders.service';
+
+import { OrdersTable }
+from '@/domains/orders/components/orders-table';
+
+export default async function OrdersPage() {
+
+  const orders = await fetchOrders();
 
   return (
 
-    <main>
+    <main className="space-y-6">
 
-      <h1>
-        Pedidos
-      </h1>
+      <div>
+
+        <h1 className="text-4xl font-bold">
+
+          Pedidos
+
+        </h1>
+
+      </div>
+
+      <OrdersTable
+        orders={orders}
+      />
 
     </main>
   );

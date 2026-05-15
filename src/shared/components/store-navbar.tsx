d450439bdf103f216621ce668ec26c7be4ed0db5
crumafood-create <crumafood@gmail.com>
2026-05-15 00:25:55 -0600

    Update and rename .tsx to store-navbar.tsx

---
+++ src/shared/components/store-navbar.tsx
@@ -0,0 +1,17 @@
+export function StoreNavbar() {
+
+  return (
+
+    <header className="border-b">
+
+      <div className="mx-auto flex h-16 max-w-7xl items-center px-6">
+
+        <h2>
+          Crumafood
+        </h2>
+
+      </div>
+
+    </header>
+  );
+}

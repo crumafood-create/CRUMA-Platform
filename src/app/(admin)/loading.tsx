5c63766704611079d263c243fde399f31d3577df
crumafood-create <crumafood@gmail.com>
2026-05-15 14:35:24 -0600

    Create loading.tsx

---
+++ src/app/(admin)/loading.tsx
@@ -0,0 +1,15 @@
+export default function AdminLoading() {
+
+  return (
+
+    <main className="flex min-h-screen items-center justify-center">
+
+      <p>
+
+        Cargando admin...
+
+      </p>
+
+    </main>
+  );
+}

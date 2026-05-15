4bf56668f6f5c4740ad61e84b9391ab1ec9711e7
crumafood-create <crumafood@gmail.com>
2026-05-15 00:46:02 -0600

    Update page.tsx

--- src/app/(storefront)/catalogo/page.tsx
+++ src/app/(storefront)/catalogo/page.tsx
@@ -1,4 +1,9 @@
-export default function CatalogPage() {
+import { fetchProducts }
+from '@/domains/catalog/services/catalog.service';
+
+export default async function CatalogPage() {
+
+  const products = await fetchProducts();
 
   return (
 
@@ -7,6 +12,10 @@ export default function CatalogPage() {
       <h1>
         Catálogo
       </h1>
+
+      <pre>
+        {JSON.stringify(products, null, 2)}
+      </pre>
 
     </main>
   );

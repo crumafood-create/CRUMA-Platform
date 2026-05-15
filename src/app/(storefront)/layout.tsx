44c7c1016795fc92d4e9bcd88dd4da1ab004947f
crumafood-create <crumafood@gmail.com>
2026-05-14 23:48:39 -0600

    Update layout.tsx

--- src/app/(storefront)/layout.tsx
+++ src/app/(storefront)/layout.tsx
@@ -1,1 +1,27 @@
+import type { ReactNode } from 'react';
 
+export default function StorefrontLayout({
+  children
+}: {
+  children: ReactNode;
+}) {
+
+  return (
+
+    <div>
+
+      <header>
+        Navbar
+      </header>
+
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+
+    </div>
+  );
+}

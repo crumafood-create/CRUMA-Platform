cc23baace56cac993f4b5a8b4f0231d7cfd1cec0
crumafood-create <crumafood@gmail.com>
2026-05-14 23:15:46 -0600

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,1 +1,23 @@
+'use client';
 
+export default function ErrorPage({
+  error
+}: {
+  error: Error;
+}) {
+
+  return (
+
+    <div>
+
+      <h2>
+        Error inesperado
+      </h2>
+
+      <p>
+        {error.message}
+      </p>
+
+    </div>
+  );
+}

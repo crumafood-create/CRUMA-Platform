c8c7454c399ad0008f4d195338d373086e48fd64
crumafood-create <crumafood@gmail.com>
2026-05-15 09:47:54 -0600

    Update and rename .tsx to orders.repository.ts

---
+++ src/domains/orders/repositories/orders.repository.ts
@@ -0,0 +1,35 @@
+import { createClient }
+from '@/infrastructure/supabase/server';
+
+import { orderDto }
+from '../dto/order.dto';
+
+export async function getOrders() {
+
+  const supabase = await createClient();
+
+  const { data, error } =
+    await supabase
+
+      .from('orders')
+
+      .select(`
+        id,
+        status,
+        payment_status,
+        total_amount,
+        created_at,
+        full_name,
+        phone
+      `)
+
+      .order('created_at', {
+        ascending: false
+      });
+
+  if (error) {
+    throw error;
+  }
+
+  return data.map(orderDto);
+}

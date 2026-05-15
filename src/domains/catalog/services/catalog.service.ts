754bd36667deccbfc13388d39898a5473516ea18
crumafood-create <crumafood@gmail.com>
2026-05-15 00:44:50 -0600

    Update and rename .tsx to catalog.service.ts

---
+++ src/domains/catalog/services/catalog.service.ts
@@ -0,0 +1,17 @@
+import {
+  getProducts,
+  getProductBySlug
+}
+from '../repositories/catalog.repository';
+
+export async function fetchProducts() {
+
+  return getProducts();
+}
+
+export async function fetchProduct(
+  slug: string
+) {
+
+  return getProductBySlug(slug);
+}

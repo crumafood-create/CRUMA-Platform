885237e338cfc39a90f1d566a09177a55269fcee
crumafood-create <crumafood@gmail.com>
2026-05-14 23:32:45 -0600

    Create get-user.ts

---
+++ src/domains/auth/services/get-user.ts
@@ -0,0 +1,14 @@
+import { createClient }
+from '@/infrastructure/supabase/server';
+
+export async function getUser() {
+
+  const supabase = await createClient();
+
+  const {
+
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  return user;
+}

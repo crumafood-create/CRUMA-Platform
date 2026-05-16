cc01b75bc3a6c77a4dcc4630c3c5cffded18b451
crumafood-create <crumafood@gmail.com>
2026-05-16 08:57:35 -0600

    Create segmentation.service.ts

---
+++ src/domains/marketing/services/segmentation.service.ts
@@ -0,0 +1,18 @@
+export function segmentCustomers(
+  customers: any[]
+) {
+
+  return {
+
+    vip: customers.filter(
+      customer =>
+        customer.lifetime_value > 1000
+    ),
+
+    regular: customers.filter(
+      customer =>
+        customer.lifetime_value <= 1000
+    )
+  };
+}
+

9a2a498e09de69a159f180a26638dd73f093a0de
crumafood-create <crumafood@gmail.com>
2026-05-16 13:46:15 -0600

    Update section.tsx

--- src/shared/ui/section.tsx
+++ src/shared/ui/section.tsx
@@ -1,1 +1,21 @@
+import type { ReactNode }
+from 'react';
 
+interface Props {
+
+  children: ReactNode;
+}
+
+export function Section({
+  children
+}: Props) {
+
+  return (
+
+    <section className="space-y-6">
+
+      {children}
+
+    </section>
+  );
+}

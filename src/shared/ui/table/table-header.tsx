657385f5703d8bc83a9e5b3b8cb6be4ee27ab8cf
crumafood-create <crumafood@gmail.com>
2026-05-17 20:22:58 -0600

    Update table-header.tsx

--- src/shared/ui/table/table-header.tsx
+++ src/shared/ui/table/table-header.tsx
@@ -1,1 +1,21 @@
+import type { ReactNode }
+from 'react';
 
+interface Props {
+
+  children: ReactNode;
+}
+
+export function TableHeader({
+  children
+}: Props) {
+
+  return (
+
+    <thead className="border-b bg-gray-50">
+
+      {children}
+
+    </thead>
+  );
+}

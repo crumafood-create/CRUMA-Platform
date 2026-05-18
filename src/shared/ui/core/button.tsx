4381f3744977d4c58c198359939bb22a510f9b1c
crumafood-create <crumafood@gmail.com>
2026-05-18 15:41:22 -0600

    Create reusable Button component in button.tsx

--- src/shared/ui/core/button.tsx
+++ src/shared/ui/core/button.tsx
@@ -1,1 +1,29 @@
+import type { ButtonHTMLAttributes }
+from 'react';
 
+interface Props
+extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button({
+  className = '',
+  ...props
+}: Props) {
+
+  return (
+
+    <button
+      {...props}
+      className={`
+        rounded-xl
+        bg-black
+        px-4
+        py-2
+        text-white
+        transition
+        hover:opacity-90
+        disabled:opacity-50
+        ${className}
+      `}
+    />
+  );
+}

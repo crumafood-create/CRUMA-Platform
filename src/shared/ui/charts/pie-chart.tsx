bbf5252b967c86e4be500a8bc51a4aa6e2b0fcd5
crumafood-create <crumafood@gmail.com>
2026-05-17 22:59:02 -0600

    Update pie-chart.tsx

--- src/shared/ui/charts/pie-chart.tsx
+++ src/shared/ui/charts/pie-chart.tsx
@@ -1,1 +1,18 @@
+interface Props {
 
+  data?: unknown[];
+}
+
+export function PieChart({
+  data
+}: Props) {
+
+  return (
+
+    <div className="flex h-[300px] items-center justify-center rounded-xl border border-dashed">
+
+      Pie Chart
+
+    </div>
+  );
+}

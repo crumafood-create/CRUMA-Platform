ea7d826d39e3e701185f2021b522c7a43b48ac17
crumafood-create <crumafood@gmail.com>
2026-05-19 08:44:31 -0600

    Delete src/domains/crm/services directory

--- src/domains/crm/services/crm.service.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only';
-
-import { getCustomerProfiles }
-from '../repositories/crm.repository';
-
-export async function fetchCustomerProfiles() {
-
-  return getCustomerProfiles();
-}

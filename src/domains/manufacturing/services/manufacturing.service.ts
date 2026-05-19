c751b9229503c2e0bebf52ac5d40fa578ece645d
crumafood-create <crumafood@gmail.com>
2026-05-19 10:43:01 -0600

    Delete src/domains/manufacturing directory

--- src/domains/manufacturing/services/manufacturing.service.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only';
-
-import { getProductionOrders }
-from '../repositories/manufacturing.repository';
-
-export async function fetchProductionOrders() {
-
-  return getProductionOrders();
-}

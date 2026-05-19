02c4886d1dcf7d7f2ba038694b8e90e93e7a120f
crumafood-create <crumafood@gmail.com>
2026-05-19 10:42:33 -0600

    Delete src/domains/inventory directory

--- src/domains/inventory/services/inventory.service.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only';
-
-import { getInventoryLevels }
-from '../repositories/inventory.repository';
-
-export async function fetchInventoryLevels() {
-
-  return getInventoryLevels();
-}

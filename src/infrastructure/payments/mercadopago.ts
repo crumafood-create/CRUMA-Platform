7b9793b80190cd9c8f9c39c6d02ebef81ee03a01
crumafood-create <crumafood@gmail.com>
2026-05-19 08:08:28 -0600

    Delete src/infrastructure/payments/mercadopago.ts

--- src/infrastructure/payments/mercadopago.ts
+++
@@ -1,7 +0,0 @@
-import { MercadoPagoConfig, Preference } from 'mercadopago';
-
-// Cliente oficial inicializado con tu Access Token de Vercel
-export const client = new MercadoPagoConfig({ 
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
-  options: { timeout: 5000 } 
-});

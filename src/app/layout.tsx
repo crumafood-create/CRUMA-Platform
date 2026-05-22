8b28c8d5d463db53e50c086241e5ee3ef9c07896
crumafood-create <crumafood@gmail.com>
2026-05-22 07:29:51 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
         <AppProvider>
           {children}

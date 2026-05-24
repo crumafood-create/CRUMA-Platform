22663d140005c92dc9b3a5d1b66e6eca280d2848
crumafood-create <crumafood@gmail.com>
2026-05-24 08:38:12 -0600

    Update toast-provider.tsx

--- src/providers/toast-provider.tsx
+++ src/providers/toast-provider.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useTheme } from 'next-themes';
 import { Toaster } from 'sonner';
 
 export function ToastProvider() {
+  const { theme = 'system' } = useTheme();
+
   return (
     <Toaster
+      theme={theme as 'light' | 'dark' | 'system'}
       richColors
       position="top-right"
       closeButton

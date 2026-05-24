fbf391b50f8640ad9361aeb356c0ad7f6b75fdd1
crumafood-create <crumafood@gmail.com>
2026-05-24 08:33:22 -0600

    Update query-provider.tsx

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,1 +1,41 @@
+'use client';
 
+import { useState } from 'react';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+export function QueryProvider({
+  children,
+}: QueryProviderProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60,
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  );
+}

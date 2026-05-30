400d58420c56eb4a718363ff398abe42b6ad445c
crumafood-create <crumafood@gmail.com>
2026-05-30 08:56:06 -0600

    Update form-input.tsx

--- src/shared/forms/components/form-input.tsx
+++ src/shared/forms/components/form-input.tsx
@@ -1,1 +1,32 @@
+import type {
+  InputHTMLAttributes
+}
+from 'react';
 
+interface Props
+extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function FormInput({
+  className = '',
+  ...props
+}: Props) {
+
+  return (
+
+    <input
+      {...props}
+      className={`
+        w-full
+        rounded-xl
+        border
+        px-4
+        py-3
+        outline-none
+        transition
+        focus:ring-2
+        focus:ring-black
+        ${className}
+      `}
+    />
+  );
+}

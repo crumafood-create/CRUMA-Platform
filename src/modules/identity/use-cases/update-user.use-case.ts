d039c6de47f2117dad8fec16370ff97a4b76aae7
crumafood-create <crumafood@gmail.com>
2026-05-31 15:21:02 -0600

    Update update-user.use-case.ts

--- src/modules/identity/use-cases/update-user.use-case.ts
+++ src/modules/identity/use-cases/update-user.use-case.ts
@@ -1,1 +1,10 @@
+import { UserSchema } from '../../schemas';
 
+export class UpdateUserUseCase {
+  execute(data: unknown) {
+    const user =
+      UserSchema.parse(data);
+
+    return user;
+  }
+}

d2ac16f75ed7908623641c981cb3b29761eb4de8
crumafood-create <crumafood@gmail.com>
2026-05-31 15:36:07 -0600

    Update user.repository.ts

--- src/modules/identity/application/contracts/user.repository.ts
+++ src/modules/identity/application/contracts/user.repository.ts
@@ -1,1 +1,22 @@
+import { UserDto } from '../../schemas';
 
+export interface UserRepository {
+  create(
+    user: UserDto
+  ): Promise<UserDto>;
+
+  update(
+    id: string,
+    user: UserDto
+  ): Promise<UserDto>;
+
+  delete(
+    id: string
+  ): Promise<void>;
+
+  findById(
+    id: string
+  ): Promise<UserDto | null>;
+
+  findAll(): Promise<UserDto[]>;
+}

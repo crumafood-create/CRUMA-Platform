9d21f4fa13a8b58cdd7b2305a4732b4b5c348f95
crumafood-create <crumafood@gmail.com>
2026-05-31 07:34:33 -0600

    Update form-textarea.tsx

--- src/shared/ui/forms/controls/form-textarea.tsx
+++ src/shared/ui/forms/controls/form-textarea.tsx
@@ -1,1 +1,33 @@
+import type {
+  TextareaHTMLAttributes
+}
+from 'react';
 
+interface Props
+extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+export function FormTextarea({
+  className = '',
+  ...props
+}: Props) {
+
+  return (
+
+    <textarea
+      {...props}
+      className={`
+        min-h-[120px]
+        w-full
+        rounded-xl
+        border
+        px-4
+        py-3
+        outline-none
+        transition
+        focus:ring-2
+        focus:ring-black
+        ${className}
+      `}
+    />
+  );
+}

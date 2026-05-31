68009fb3a6217dcf043390bf7ae2b44aa948f5fa
crumafood-create <crumafood@gmail.com>
2026-05-31 11:29:15 -0600

    Update index.ts

--- src/shared/ui/data-table/features/index.ts
+++ src/shared/ui/data-table/features/index.ts
@@ -1,1 +1,6 @@
-
+export * from './table-column-toggle';
+export * from './table-export';
+export * from './table-pagination';
+export * from './table-selection';
+export * from './table-sort';
+export * from './table-view-options';

982bfb964eee497f740f436c9fadeab3e7bd3c21
crumafood-create <crumafood@gmail.com>
2026-05-31 14:22:03 -0600

    Update index.ts

--- src/modules/identity/domain/index.ts
+++ src/modules/identity/domain/index.ts
@@ -1,1 +1,10 @@
+export * from './entities/tenant.entity';
+export * from './entities/role.entity';
+export * from './entities/permission.entity';
 
+export * from './value-objects/tenant-id.vo';
+export * from './value-objects/role-id.vo';
+export * from './value-objects/permission-id.vo';
+export * from './value-objects/user-id.vo';
+export * from './value-objects/email.vo';
+export * from './value-objects/slug.vo';

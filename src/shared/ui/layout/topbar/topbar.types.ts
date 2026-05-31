c5a882b4311e7cb56422e03a38c23d31e00029ca
crumafood-create <crumafood@gmail.com>
2026-05-31 09:49:51 -0600

    Update topbar.types.ts

--- src/shared/ui/layout/topbar/topbar.types.ts
+++ src/shared/ui/layout/topbar/topbar.types.ts
@@ -1,1 +1,9 @@
+import type { ReactNode } from 'react';
 
+export interface TopbarProps {
+  title?: string;
+  breadcrumbs?: ReactNode;
+  actions?: ReactNode;
+  userMenu?: ReactNode;
+  className?: string;
+}

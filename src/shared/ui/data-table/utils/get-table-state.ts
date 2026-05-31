fbad61bed5d2c8d69b5604ee98ae17ad4eb337b1
crumafood-create <crumafood@gmail.com>
2026-05-31 11:35:55 -0600

    Update get-table-state.ts

--- src/shared/ui/data-table/utils/get-table-state.ts
+++ src/shared/ui/data-table/utils/get-table-state.ts
@@ -1,1 +1,19 @@
+export type TableState =
+  | 'loading'
+  | 'empty'
+  | 'success';
 
+export function getTableState(
+  isLoading: boolean,
+  rowsCount: number
+): TableState {
+  if (isLoading) {
+    return 'loading';
+  }
+
+  if (rowsCount === 0) {
+    return 'empty';
+  }
+
+  return 'success';
+}

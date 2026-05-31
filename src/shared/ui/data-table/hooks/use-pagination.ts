1b460fef10ee990bd4d8e8c132e9d51d633fc9d1
crumafood-create <crumafood@gmail.com>
2026-05-31 11:42:02 -0600

    Update use-pagination.ts

--- src/shared/ui/data-table/hooks/use-pagination.ts
+++ src/shared/ui/data-table/hooks/use-pagination.ts
@@ -1,1 +1,16 @@
+import { useState } from 'react';
 
+export function usePagination() {
+  const [page, setPage] =
+    useState(1);
+
+  const [pageSize, setPageSize] =
+    useState(10);
+
+  return {
+    page,
+    pageSize,
+    setPage,
+    setPageSize,
+  };
+}

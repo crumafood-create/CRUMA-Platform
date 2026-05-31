633447f0feebe07e27d7153354f84d379d8aaf30
crumafood-create <crumafood@gmail.com>
2026-05-31 15:31:31 -0600

    Update create-user.use-case.ts

--- src/modules/identity/application/use-cases/create-user.use-case.ts
+++ src/modules/identity/application/use-cases/create-user.use-case.ts
@@ -1,1 +1,10 @@
+import { UserSchema } from '../../schemas';
 
+export class CreateUserUseCase {
+  execute(data: unknown) {
+    const user =
+      UserSchema.parse(data);
+
+    return user;
+  }
+}

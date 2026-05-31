f332bb84dbf0e6c3374c3efc80ac66e79aab381e
crumafood-create <crumafood@gmail.com>
2026-05-31 15:38:24 -0600

    Update tenant.repository.ts

--- src/modules/identity/application/contracts/tenant.repository.ts
+++ src/modules/identity/application/contracts/tenant.repository.ts
@@ -1,1 +1,11 @@
+import { TenantDto } from '../../schemas';
 
+export interface TenantRepository {
+  findAll(): Promise<
+    TenantDto[]
+  >;
+
+  findById(
+    id: string
+  ): Promise<TenantDto | null>;
+}

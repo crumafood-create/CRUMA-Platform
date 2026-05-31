1ec260f94a820c419290ecd99356901d270b61b8
crumafood-create <crumafood@gmail.com>
2026-05-31 12:13:27 -0600

    Update table-header.tsx

--- src/shared/ui/data-table/core/table-header.tsx
+++ src/shared/ui/data-table/core/table-header.tsx
@@ -1,1 +1,15 @@
+interface Props {
+  title?: string;
+}
 
+export function TableHeader({
+  title,
+}: Props) {
+  return (
+    <div className="flex items-center justify-between py-4">
+      <h3 className="font-medium">
+        {title}
+      </h3>
+    </div>
+  );
+}

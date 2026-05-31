ea946764a8ce8ed9293e3e42724ccd9298015279
crumafood-create <crumafood@gmail.com>
2026-05-31 11:36:56 -0600

    Update use-table.ts

--- src/shared/ui/data-table/hooks/use-table.ts
+++ src/shared/ui/data-table/hooks/use-table.ts
@@ -1,1 +1,16 @@
+import {
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
+export function useTable({
+  data,
+  columns,
+}: any) {
+  return useReactTable({
+    data,
+    columns,
+    getCoreRowModel:
+      getCoreRowModel(),
+  });
+}

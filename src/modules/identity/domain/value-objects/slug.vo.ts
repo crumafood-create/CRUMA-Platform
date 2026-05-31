ef56e4a19bedaa5e6879a9db0e9ed7e0689abddc
crumafood-create <crumafood@gmail.com>
2026-05-31 14:12:21 -0600

    Update slug.vo.ts

--- src/modules/identity/domain/value-objects/slug.vo.ts
+++ src/modules/identity/domain/value-objects/slug.vo.ts
@@ -1,1 +1,24 @@
+export class Slug {
+  private readonly value: string;
 
+  constructor(value: string) {
+    const slug = value
+      .trim()
+      .toLowerCase()
+      .replace(/\s+/g, '-');
+
+    if (!slug.length) {
+      throw new Error('Invalid slug');
+    }
+
+    this.value = slug;
+  }
+
+  toString(): string {
+    return this.value;
+  }
+
+  equals(other: Slug): boolean {
+    return this.value === other.value;
+  }
+}

c8037e1b95330485c0881afebb2acc35509df9dd
crumafood-create <crumafood@gmail.com>
2026-05-31 11:48:34 -0600

    Update table-toolbar.tsx

--- src/shared/ui/data-table/toolbar/table-toolbar.tsx
+++ src/shared/ui/data-table/toolbar/table-toolbar.tsx
@@ -1,1 +1,13 @@
+interface Props {
+  children?: React.ReactNode;
+}
 
+export function TableToolbar({
+  children,
+}: Props) {
+  return (
+    <div className="flex items-center justify-between gap-4 py-4">
+      {children}
+    </div>
+  );
+}

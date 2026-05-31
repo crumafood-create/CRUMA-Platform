8f01a99754ce4558d0f1373ea88547d47af5f27e
crumafood-create <crumafood@gmail.com>
2026-05-31 14:11:50 -0600

    Update email.vo.ts

--- src/modules/identity/domain/value-objects/email.vo.ts
+++ src/modules/identity/domain/value-objects/email.vo.ts
@@ -1,1 +1,24 @@
+export class Email {
+  private readonly value: string;
 
+  constructor(value: string) {
+    const email = value.trim().toLowerCase();
+
+    const regex =
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regex.test(email)) {
+      throw new Error('Invalid email');
+    }
+
+    this.value = email;
+  }
+
+  toString(): string {
+    return this.value;
+  }
+
+  equals(other: Email): boolean {
+    return this.value === other.value;
+  }
+}

f1a4c2a14ba8259018a9eb779dd65955cc840c27
crumafood-create <crumafood@gmail.com>
2026-05-31 19:29:48 -0600

    Implement useUsers hook with data fetching

--- src/modules/identity/presentation/users/hooks/use-users.ts
+++ src/modules/identity/presentation/users/hooks/use-users.ts
@@ -1,1 +1,38 @@
+'use client';
 
+import { useMemo } from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+
+import {
+  ListUsersUseCase,
+} from '@/modules/identity/application/use-cases';
+
+export function useUsers() {
+  const query = useQuery({
+    queryKey: ['users'],
+
+    queryFn: async () => {
+      const useCase =
+        new ListUsersUseCase();
+
+      return useCase.execute();
+    },
+  });
+
+  return useMemo(
+    () => ({
+      users: query.data ?? [],
+
+      isLoading:
+        query.isLoading,
+
+      isFetching:
+        query.isFetching,
+
+      refetch:
+        query.refetch,
+    }),
+    [query]
+  );
+}

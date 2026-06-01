c9c7cc3fc2cba8655f816c4422ce1637404aac37
crumafood-create <crumafood@gmail.com>
2026-05-31 19:30:43 -0600

    Update use-user-form.ts

--- src/modules/identity/presentation/users/hooks/use-user-form.ts
+++ src/modules/identity/presentation/users/hooks/use-user-form.ts
@@ -1,1 +1,54 @@
+'use client';
 
+import { useMutation } from '@tanstack/react-query';
+
+import {
+  CreateUserUseCase,
+  UpdateUserUseCase,
+} from '@/modules/identity/application/use-cases';
+
+import {
+  UserDto,
+} from '@/modules/identity/schemas';
+
+interface Props {
+  userId?: string;
+}
+
+export function useUserForm({
+  userId,
+}: Props = {}) {
+  const mutation =
+    useMutation({
+      mutationFn: async (
+        values: UserDto
+      ) => {
+        if (userId) {
+          const useCase =
+            new UpdateUserUseCase();
+
+          return useCase.execute(
+            values
+          );
+        }
+
+        const useCase =
+          new CreateUserUseCase();
+
+        return useCase.execute(
+          values
+        );
+      },
+    });
+
+  return {
+    submit:
+      mutation.mutateAsync,
+
+    isLoading:
+      mutation.isPending,
+
+    error:
+      mutation.error,
+  };
+}

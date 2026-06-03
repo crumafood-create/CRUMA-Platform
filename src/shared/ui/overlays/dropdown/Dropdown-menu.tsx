05b391207fd0f29c5f90348ab4f10298a8a98233
crumafood-create <crumafood@gmail.com>
2026-06-03 08:16:47 -0600

    Update and rename .ts to Dropdown-menu.tsx

---
+++ src/shared/ui/overlays/dropdown/Dropdown-menu.tsx
@@ -0,0 +1,33 @@
+import type { ReactNode } from 'react';
+
+export function DropdownMenu({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>{children}</div>;
+}
+
+export function DropdownMenuTrigger({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>{children}</div>;
+}
+
+export function DropdownMenuContent({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>{children}</div>;
+}
+
+export function Item DropdownMenu({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>{children}</div>;
+}

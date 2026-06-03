29f987c3bdc6dcb97b917430bc85e7ced44e3509
crumafood-create <crumafood@gmail.com>
2026-06-03 09:12:11 -0600

    Update mobile-navbar.tsx

--- src/app/(admin)/_components/navigation/mobile-navbar.tsx
+++ src/app/(admin)/_components/navigation/mobile-navbar.tsx
@@ -1,36 +1,7 @@
-'use client';
-
-import Link
-from 'next/link';
-
-export function MobileNavbar() {
-
+export function AdminNavbar() {
   return (
-
-    <nav className="fixed bottom-0 left-0 right-0 border-t bg-white p-4 md:hidden">
-
-      <div className="flex justify-around">
-
-        <Link href="/">
-
-          Inicio
-
-        </Link>
-
-        <Link href="/catalogo">
-
-          Catálogo
-
-        </Link>
-
-        <Link href="/carrito">
-
-          Carrito
-
-        </Link>
-
-      </div>
-
-    </nav>
+    <header className="flex items-center justify-between border-b p-4">
+      <h2>Admin</h2>
+    </header>
   );
 }

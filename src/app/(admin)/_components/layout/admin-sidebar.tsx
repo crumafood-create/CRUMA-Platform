0c43fbc01afd666283c440caecff5e735d7c5bda
crumafood-create <crumafood@gmail.com>
2026-06-03 07:06:48 -0600

    Update admin-sidebar.tsx

--- src/app/(admin)/_components/layout/admin-sidebar.tsx
+++ src/app/(admin)/_components/layout/admin-sidebar.tsx
@@ -1,1 +1,13 @@
+import type { ReactNode } from 'react';
 
+export function AdminSidebar({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}

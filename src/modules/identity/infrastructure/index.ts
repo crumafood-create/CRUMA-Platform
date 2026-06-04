8dddbb011285f57da7ddb12214633a8ad802bd7c
crumafood-create <crumafood@gmail.com>
2026-06-03 22:20:59 -0600

    Update index.ts

--- src/modules/identity/infrastructure/index.ts
+++ src/modules/identity/infrastructure/index.ts
@@ -1,4 +1,1 @@
-// export * from './repositories/prisma-user.repository';
-// export * from './repositories/prisma-role.repository';
-// export * from './repositories/prisma-permission.repository';
-// export * from './repositories/prisma-tenant.repository';
+

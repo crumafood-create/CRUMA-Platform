3096ddc1bd8a54f00043eee61f1a2ed9301ba723
crumafood-create <crumafood@gmail.com>
2026-06-03 18:13:37 -0600

    Update server.ts

--- src/infrastructure/integrations/supabase/server.ts
+++ src/infrastructure/integrations/supabase/server.ts
@@ -19,7 +19,13 @@ export async function createClient() {
           return cookieStore.getAll();
         },
 
-        setAll(cookiesToSet) {
+        setAll(
+  cookiesToSet: Array<{
+    name: string;
+    value: string;
+    options?: any;
+  }>
+) {
 
           try {
 

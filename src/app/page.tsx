48041dbdb902a961bd06551d364381d90b32251e
crumafood-create <crumafood@gmail.com>
2026-06-05 10:58:08 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@
 
 import { toast } from 'sonner';
 
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+  redirect('/login');
+}
+
 export default function HomePage() {
   return (
     <main className="p-10">

c5418ea2aa49cfc848deb719dc90db83532c4a96
crumafood-create <crumafood@gmail.com>
2026-06-08 15:28:58 -0600

    Update admin-sidebar.tsx

--- src/app/(admin)/_components/admin-sidebar.tsx
+++ src/app/(admin)/_components/admin-sidebar.tsx
@@ -19,6 +19,10 @@ export default function AdminSidebar() {
         <Link href="/products">
           Productos
         </Link>
+
+        <Link href="/categories">
+  Categorías
+</Link>
       </nav>
     </aside>
   );

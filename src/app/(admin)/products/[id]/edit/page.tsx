7d4eb09989ae62762b72dfe23a7ef19475413259
crumafood-create <crumafood@gmail.com>
2026-06-08 14:47:01 -0600

    Update page.tsx

--- src/app/(admin)/products/[id]/edit/page.tsx
+++ src/app/(admin)/products/[id]/edit/page.tsx
@@ -6,21 +6,6 @@ import { ProductForm } from '@/app/(admin)/_components/product-form';
 
 import { updateProduct } from '../../../actions';
 
-export async function deleteProduct(
-  productId: string
-) {
-  const supabase = await createClient();
-
-  await supabase
-    .from('products')
-    .update({
-      deleted_at: new Date(),
-    })
-    .eq('id', productId);
-
-  revalidatePath('/products');
-}
-
 export default async function EditProductPage({
   params,
 }: {

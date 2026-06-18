473ed226d1ed81db2fe031920a74edc55bd73d6b
crumafood-create <crumafood@gmail.com>
2026-06-18 12:42:51 -0600

    Create NewUnitPage for unit of measure form

---
+++ src/app/(admin)/units-of-measure/new/page.tsx
@@ -0,0 +1,19 @@
+import { UnitOfMeasureForm }
+from '@/app/(admin)/_components/unit-of-measure-form';
+
+import { createUnitOfMeasure }
+from '../actions';
+
+export default function NewUnitPage() {
+  return (
+    <main className="space-y-6">
+      <h1 className="text-4xl font-bold">
+        Nueva Unidad
+      </h1>
+
+      <UnitOfMeasureForm
+        action={createUnitOfMeasure}
+      />
+    </main>
+  );
+}

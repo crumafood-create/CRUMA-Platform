fb5d9ce52adc81a047882e3ac853db35b91539f6
crumafood-create <crumafood@gmail.com>
2026-06-19 13:12:07 -0600

    Update page.tsx

--- src/app/(admin)/recipes/[id]/page.tsx
+++ src/app/(admin)/recipes/[id]/page.tsx
@@ -81,7 +81,7 @@ export default async function RecipePage({
               >
                 <div>
                   {
-                    item.products?.name
+                    item.raw_materials?.name
                   }
                 </div>
 

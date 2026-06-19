abfabcf8a339bc4ae859f8ce68c494ed9c317c3f
crumafood-create <crumafood@gmail.com>
2026-06-19 14:55:41 -0600

    Update page.tsx

--- src/app/(admin)/products/page.tsx
+++ src/app/(admin)/products/page.tsx
@@ -1,3 +1,24 @@
+const { data: products, error } = await supabase
+  .from('products')
+  .select('*');
+
+console.log('ERROR:', error);
+console.log('PRODUCTS:', products);return (
+  <main>
+    <pre>
+      {JSON.stringify(
+        {
+          error,
+          count: products?.length,
+          products,
+        },
+        null,
+        2
+      )}
+    </pre>
+  </main>
+);
+
 return (
   <main>
     <pre>

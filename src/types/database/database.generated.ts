7eadf29ecc85f92f1348552182fddf5c9b8bf1a1
crumafood-create <crumafood@gmail.com>
2026-09-17 18:04:33 +0000

    fix(db): include public schema in generated database types

--- src/types/database/database.generated.ts
+++ src/types/database/database.generated.ts
@@ -7,31 +7,6 @@ export type Json =
   | Json[]
 
 export type Database = {
-  graphql_public: {
-    Tables: {
-      [_ in never]: never
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      graphql: {
-        Args: {
-          extensions?: Json
-          operationName?: string
-          query?: string
-          variables?: Json
-        }
-        Returns: Json
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
   public: {
     Tables: {
       abandoned_carts: {
@@ -7869,9 +7844,6 @@ export type CompositeTypes<
     : never
 
 export const Constants = {
-  graphql_public: {
-    Enums: {},
-  },
   public: {
     Enums: {
       payment_method_type: [
